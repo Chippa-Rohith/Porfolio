22034c9a3ef70c1d17269096c4cea472931d4ba7
Chippa Rohith <64948774+Chippa-Rohith@users.noreply.github.com>
2025-05-31 16:48:58 -0700

    updated data anlaysis title

--- src/components/skills/DataAnalysis.jsx
+++ src/components/skills/DataAnalysis.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const DataAnalysis = () => {
   return (
     <div className="skills__content">
-      <h3 className="skills__title">Frontend</h3>
+      <h3 className="skills__title">Data Analysis</h3>
 
       <div className="skills__box">
         Extracting insights from complex datasets using various statistical methods and visualizations. Skilled in Python and SQL for exploratory data analysis, identifying trends, and integrating machine learning solutions. Experienced in creating interactive dashboards using tools like Power BI, and QuickSight to enhance decision-making and improve business efficiency. 
