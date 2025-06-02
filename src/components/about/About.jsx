6a1eb024ad3675231f5d3d271ed541c0fa6d0091
Chippa Rohith <64948774+Chippa-Rohith@users.noreply.github.com>
2025-06-02 12:15:15 -0700

    updated resume file

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import ProfileImg from "../../assets/Profile.jpeg";
-import Resume from "../../assets/ROHITH_RESUME_2025.pdf";
+import Resume from "../../assets/ROHITH_RESUME_2k25.pdf";
 import Info from "./Info";
 
 const About = () => {
